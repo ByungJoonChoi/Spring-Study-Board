f1d23155adde28a8644fa1c6532bef1651672add
Peter Choi <cbj0618@gmail.com>
2018-01-18 01:32:17 +0900

    <주요 내용 : /register (GET, POST) 라우터 추가> 1. BoardController에 /register request mapping 추가 2. register.jsp 추가   - title, content, writer를 입력받아 서버로 post 방식으로 전송하는 페이지 3. success.jsp 추가   - POST /register 서버 로직 처리후, 게시판이 성공적으로 등록되었음을 알리는 페이지

--- src/main/java/io/peter/controller/BoardController.java
+++ src/main/java/io/peter/controller/BoardController.java
@@ -5,8 +5,11 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.peter.domain.BoardVO;
 import io.peter.service.BoardService;
 
 @Controller
@@ -17,4 +20,23 @@ public class BoardController {
 	
 	@Inject
 	private BoardService service;
+	
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception{
+		
+		logger.info("register get .............");
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String registPOST(BoardVO board, Model model) throws Exception{
+		
+		logger.info("regist post ..............");
+		logger.info(board.toString());
+		
+		service.regist(board);
+		
+		model.addAttribute("result", "success");
+		
+		return "/board/success";
+	}
 }
