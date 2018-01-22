ec987f5a98ec58c64047f78087e49934e1a248d3
Peter Choi <cbj0618@gmail.com>
2018-01-22 17:59:53 +0900

    <주요 내용 : 리스트 화면 하단 page navigation 부분 계산을 위한 클래스 추가> - 요약 : Criteria 객체, totalCount를 외부로 부터 주입받은 뒤,   endPage, startPage, prev, next를 순차적으로 계산함.

---
+++ src/main/java/io/peter/domain/PageMaker.java
@@ -0,0 +1,86 @@
+package io.peter.domain;
+
+public class PageMaker {
+	
+	private int totalCount; // DB에 저장된 게시물의 총 갯수. 
+	private int startPage;
+	private int endPage;
+	private boolean prev;
+	private boolean next;
+	
+	private int displayPageNum = 10; // 화면 하단 page navigation에서 보여줄 페이지 번호의 갯수 
+	
+	private Criteria cri; // page, perPageNum 사용 
+	
+	public void setCri(Criteria cri) {
+		this.cri = cri;
+	}
+	
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+		
+		calcData();
+	}
+
+	/*
+	 * endPage, startPage, prev, next 계산 로직
+	 */
+	private void calcData() {
+		endPage = (int) (Math.ceil(cri.getPage()/(double) displayPageNum) * displayPageNum);
+		startPage = endPage - displayPageNum + 1;
+		int tempEndPage = (int) (Math.ceil(totalCount / (double) cri.getPerPageNum()));
+		if(endPage > tempEndPage){
+			endPage = tempEndPage;
+		}
+		prev = startPage == 1 ? false : true;
+		next = endPage * cri.getPerPageNum() < totalCount ? true : false; 
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+	public boolean isPrev() {
+		return prev;
+	}
+
+	public void setPrev(boolean prev) {
+		this.prev = prev;
+	}
+
+	public boolean isNext() {
+		return next;
+	}
+
+	public void setNext(boolean next) {
+		this.next = next;
+	}
+
+	public int getDisplayPageNum() {
+		return displayPageNum;
+	}
+
+	public void setDisplayPageNum(int displayPageNum) {
+		this.displayPageNum = displayPageNum;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public Criteria getCri() {
+		return cri;
+	}
+}
