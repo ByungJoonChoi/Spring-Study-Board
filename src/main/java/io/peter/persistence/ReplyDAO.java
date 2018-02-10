124ca2e07c47452969a9079a3391e247146bc567
BJ CHOI <cbj0618@gmail.com>
2018-02-10 22:08:18 +0900

    <주요 내용 : ReplyDAO 객체 및 mapper 작성>

---
+++ src/main/java/io/peter/persistence/ReplyDAO.java
@@ -0,0 +1,16 @@
+package io.peter.persistence;
+
+import java.util.List;
+
+import io.peter.domain.ReplyVO;
+
+public interface ReplyDAO {
+	
+	public List<ReplyVO> list(Integer bno) throws Exception;
+	
+	public void create(ReplyVO vo) throws Exception;
+	
+	public void update(ReplyVO vo) throws Exception;
+	
+	public void delete(Integer rno) throws Exception;
+}
