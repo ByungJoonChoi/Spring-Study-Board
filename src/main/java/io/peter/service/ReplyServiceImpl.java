20be4e3257fbdbb8d3c2e22571dce9bd4732c5b9
BJ CHOI <cbj0618@gmail.com>
2018-02-10 23:03:42 +0900

    <주요 내용 : 댓글 서비스 클래스 작성>

---
+++ src/main/java/io/peter/service/ReplyServiceImpl.java
@@ -0,0 +1,38 @@
+package io.peter.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import io.peter.domain.ReplyVO;
+import io.peter.persistence.ReplyDAO;
+
+@Service
+public class ReplyServiceImpl implements ReplyService {
+	
+	@Inject
+	private ReplyDAO dao;
+
+	@Override
+	public void addReply(ReplyVO vo) throws Exception {
+		dao.create(vo);
+	}
+
+	@Override
+	public List<ReplyVO> listReply(Integer bno) throws Exception {
+		return dao.list(bno);
+	}
+
+	@Override
+	public void modifyReply(ReplyVO vo) throws Exception {
+		dao.update(vo);
+	}
+
+	@Override
+	public void removeReply(Integer rno) throws Exception {
+		dao.delete(rno);
+	}
+
+}
